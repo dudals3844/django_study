ddbdaf7bbbeffd6b1b1a26fa2f726ea275b36646
choiyoungmin <dudals3844@gmail.com>
2021-11-06 02:54:11 +0900

    모델에 수정일시 추가

--- pybo/models.py
+++ pybo/models.py
@@ -8,6 +8,7 @@ class Question(models.Model):
     subject = models.CharField(max_length=200)
     content = models.TextField()
     create_date = models.DateTimeField()
+    modify_date = models.DateTimeField(null=True, blank=True)
 
     # 아래 __str__을 추가하면 객체를 불렀을떄 메모리 주소값이 아닌 제목으로 이름이 나온다.
     def __str__(self):
@@ -19,3 +20,4 @@ class Answer(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     content = models.TextField()
     create_date = models.DateTimeField()
+    modify_date = models.DateTimeField(null=True, blank=True)
